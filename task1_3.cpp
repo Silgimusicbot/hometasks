2b019c5eec6375001416119c6f9b6045d6a1a4ed
Silgimusicbot <149963247+Silgimusicbot@users.noreply.github.com>
2025-10-20 22:59:46 +0400

    Create task1_3.cpp

---
+++ task1_3.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float eded;
+    cin >> eded; 
+    cout << "Kvadrati: " << eded * eded << endl;
+    cout << "Kubu: " << eded * eded * eded << endl;
+}
